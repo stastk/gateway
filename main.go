6b8d3dabba1eab792ffe610ec426bb5410bfb728
Stas <me@stas.tk>
2023-04-07 14:07:43 +0300

    port changed

--- main.go
+++ main.go
@@ -92,6 +92,6 @@ func setupRouter() *gin.Engine {
 func main() {
 
 	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	// Listen and Server in 0.0.0.0:9990
+	r.Run(":9990")
 }
